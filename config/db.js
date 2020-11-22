11b805e2dc62d1190fd39a6b6c189fe405d06cf8
snubtac <cheng0316@gmail.com>
2020-11-22 10:08:53 +0800

    Initial commit

---
+++ config/db.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+      useUnifiedTopology: true,
+    });
+
+    console.log('MongoDB Connected...');
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1); // Exit with failure
+  }
+};
+
+module.exports = connectDB;
